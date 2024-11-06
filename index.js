cf88636d75f55d5770459c474bda203d8de1f664
ryanyychen <ryan_yychen@hotmail.com>
2024-11-05 21:58:25 -0800

    initial changes

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ $(document).ready(function() {
 		$("#icon").css({display: "block"});
 	}
 	
+	// Get the button:
+	let scrollButton = document.getElementById("scrollTop");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			scrollButton.style.display = "block";
+		} else {
+			scrollButton.style.display = "none";
+		}
+	}
+
+	scrollButton.addEventListener("click", toTop);
+
+	// When the user clicks on the button, scroll to the top of the document
+	function toTop() {
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
 			
 	const height = $(window).height();
 	const width = $(window).width();
